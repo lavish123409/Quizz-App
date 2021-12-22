7ad3b2e1e73b25f2d890b88a544fd985c25c5be5
Gopal-Dahale <dahalegopal27@gmail.com>
2021-12-22 11:28:45 +0530

    Resolved eslint errors

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,11 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Containers/Home';
 import Addquestion from './Containers/Addquestion';
 
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/adddetails" element={<Addquestion />} />
-    </Routes>
-  );
-};
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/adddetails" element={<Addquestion />} />
+  </Routes>
+);
 
 export default App;
